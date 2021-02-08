0107a9f9a75a1a864ee99915b98bbbb6030bd9e4
Gabriel Gonçalves <GGGguimaraesgabriel@hotmail.com>
2021-02-08 16:53:46 -0300

    Add Example of SPM

---
+++ Example - SPM/Example - SPM/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Example - SPM
+//
+//  Created by Gabriel Gonçalves Guimarães on 08/02/21.
+//
+
+import UIKit
+import CustomColorGreen
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.getCustomGreen()
+      
+    }
+
+
+}
+
